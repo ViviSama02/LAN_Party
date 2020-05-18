b5616f1af02aadcbcfa870caf96871af8fc2c405
ZeoStaze <paul.mirgaine@hotmail.fr>
2020-05-18 18:27:10 +0200

    rapport

--- resources/views/utilisateur/inscription.blade.php
+++ resources/views/utilisateur/inscription.blade.php
@@ -13,7 +13,7 @@
         <div class="card centre shadow" style="max-width: 450px;margin-top: -67px;">
             <div class="card-header">Formulaire d'inscription</div>
             <div class="card-body">
-              <form action="#">
+              {{-- <form action="#">
                   <div class="form-group">
                     <label for="emailField">Adresse email</label>
                     <input type="email" class="form-control" id="emailField" placeholder="exemple@exemple.fr">
@@ -29,7 +29,64 @@
                   <br>
                   <button type="button" class="btn btn-danger">Annuler</button>
                   <button type="submit" class="btn btn-success" style="float: right;">Valider</button>
-              </form>
+              </form> --}}
+              <form method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <div class="form-group">
+                    <label for="name">Nom</label>
+
+                    <div>
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Adresse Email</label>
+
+                    <div>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Mot de passe</label>
+
+                    <div>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password-confirm">Confirmer le mot de passe</label>
+
+                    <div>
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-success btn-lg btn-block" style="margin-top:30px">
+                    Valider
+                </button>
+            </form>
             </div>
           </div>
     </div>
