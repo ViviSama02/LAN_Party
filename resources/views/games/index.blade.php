3e33b3c5557c540bd1c011c19a6af9fb3fa632b4
ZeoStaze <paul.mirgaine@hotmail.fr>
2020-05-19 17:30:07 +0200

    game index

---
+++ resources/views/games/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" style="padding:20px">
+    <div class="row">
+        <h1 class="title">Liste des jeux :</h1>
+    </div>
+    <div class="row">
+        <ul class="list-group" style="margin: auto; min-width: 500px;">
+            @foreach($games as $game)
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <strong>{{&game->titre_jeu}}</strong>
+                <button type="button" class="btn btn-info" style="float: right;">
+                    <img src="visibility-24px.svg" alt="view icon" style="vertical-align: middle;">
+                </button>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
+@endsection
+
